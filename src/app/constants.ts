0fd329bfe5929527dbec176a81c4a1d709bc1b6b
Siddhartha Sahai <siddharthasahai99@gmail.com>
2018-03-29 17:45:47 +0530

    random node ip, anti-clickjacking

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,6 +1,12 @@
 export class Constants {
-   public static MAX_CONNECTION_ATTEMPTS = 20
-   public static CONNECTION_ATTEMPT_TIMEOUT = 600
+  public static MAX_CONNECTION_ATTEMPTS = 20
+  public static CONNECTION_ATTEMPT_TIMEOUT = 600
 
-   public static NODE_URL = 'http://localhost:4201'
+  public static NODE_URLS = [
+    'http://localhost:4201',
+    'http://localhost:4201',
+    'http://localhost:4201',
+    'http://localhost:4201',
+    'http://localhost:4201'
+  ]
 }
